2a9e21a8d36b386199345b244b964fea3fa347a6
Kinan Abbas <gerranzuv@gmail.com>
2020-04-15 11:35:12 +0300

    Add project files.

---
+++ BankApplication/Models/IdentityModels.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankApplication.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        [Display(Name = "ID")]
+        public string UserId { get; set; }
+
+        [Display(Name = "PN")]
+        public string PN { get; set; }
+
+        [Display(Name = "Fingerprint Image")]
+        public string MainImage { get; set; }
+
+        [Display(Name = "Share1 Image")]
+        public string Sahre1Image { get; set; }
+
+        [Display(Name = "Share2 Image")]
+        public string Sahre2Image { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+}
